ca2f26f1e1c7c92ae4cf814dc4f869f380eea489
dbernierQuicken <davidbernier@quickenloans.com>
2021-07-15 19:45:44 -0400

    footer and modal

---
+++ kalendarium/ClientApp/src/app/Components/footer/footer.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
+})
+
+export class FooterComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(FocResourcesComponent );
+  }
+}
+@Component({
+  selector: 'app-foc-resources',
+  templateUrl: '../foc-resources/foc-resources.component.html',
+  styleUrls: ['../foc-resources/foc-resources.component.css']
+})
+
+
+export class FocResourcesComponent{
+  constructor(){};
+}
+
