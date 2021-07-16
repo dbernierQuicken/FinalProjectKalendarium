7b8684d4fd1b057923aca18dc602a1e74323bcf9
dbernierQuicken <davidbernier@quickenloans.com>
2021-07-16 10:05:59 -0400

    hopes and prayers

--- kalendarium/ClientApp/src/app/Components/event-list/event-list.component.ts
+++ kalendarium/ClientApp/src/app/Components/event-list/event-list.component.ts
@@ -3,7 +3,6 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { Console } from 'console';
 import { KalendariumApiService } from '../../Services/kalendarium-api.service';
 import { LoginService } from '../../Services/login.service';
 
